58a0853d908312fdc8f43d5c589e07e348ccb9f3
novifitri <animangap@gmail.com>
2022-05-23 20:23:16 +0700

    init

---
+++ tugas14.js
@@ -0,0 +1,6 @@
+var bagi = 37 / 15;
+console.log("Hasil bagi 37 dengan tgl lahir = " +bagi)
+console.log("=========================")
+console.log("Ceil : " +Math.ceil(bagi))
+console.log("Round : " +Math.round(bagi))
+console.log("Floor : " +Math.floor(bagi))
